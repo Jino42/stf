1b6020cfefc93d4edf77a91b9f27c327d9571cac
Jino42 <ntoniolo@student.42.fr>
2019-07-22 15:23:08 +0200

    Add Objs

--- html/search/searchdata.js
+++ html/search/searchdata.js
@@ -1,24 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "abcdgklmprstvw",
-  1: "abcdgklmprstvw",
-  2: "s",
-  3: "alr"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "files",
-  3: "functions"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Classes",
-  2: "Files",
-  3: "Functions"
 };
 
